ca465d2815e5154f8d6795855ee72adea86a654c
lheido <leonard.treille@gmail.com>
2021-02-15 23:18:00 +0100

    Add background clouds

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('homeEnterAnimation', [
+      transition(':enter', [
+        query('nav a', [
+          style({ opacity: 0, transform: 'scale(0.7)' }),
+          stagger(100, animate('330ms linear', style({ opacity: 1, transform: 'scale(1)' })))
+        ], { optional: true }),
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
+
+  @HostBinding('@homeEnterAnimation') homeEnterAnimation = true;
 
   constructor() { }
 
