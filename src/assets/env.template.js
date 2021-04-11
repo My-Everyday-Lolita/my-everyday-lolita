2a54d954e2c84188125e0cb1e855ecb3485eb3d4
lheido <leonard.treille@gmail.com>
2021-04-11 12:20:41 +0200

    Update env template

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -4,8 +4,18 @@
     login: '${KEYCLOAK_DOMAIN}',
     mel: '${API_DOMAIN}',
   };
+  let links = {
+    patreon: '${PATREON_URL}',
+    rockuu: {
+      youtube: '${YOUTUBE_URL}',
+      youtubeLolita: '${YOUTUBE_LOLITA_URL}',
+      instagram: '${INSTAGRAM_URL}',
+      facebook: '${FACEBOOK_URL}'
+    }
+  };
   window["env"] = window["env"] || {
-    domains
+    domains,
+    links
   };
 
 })(this);
